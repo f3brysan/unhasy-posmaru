332341754980f7e1c30751068058fa76e0b49bd3
Febry San <97709170+f3brysan@users.noreply.github.com>
2025-07-29 22:31:04 +0700

    Added GuzzleHttp\Client and requestData function to Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use GuzzleHttp\Client;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function requestData($url, $method = 'GET', $data = null)
+    {
+        $option = [
+            'headers' => [
+                'Accept' => 'application/json',
+            ]
+        ];
+
+        if ($data != null) {
+            $option['form_params'] = $data;
+        }
+
+        $client = new Client();
+        $res = $client->request($method, $url, $option);
+        $stream = $res->getBody()->getContents();
+
+        $hasil = json_decode($stream);
+        return $hasil;
+    }
 }
