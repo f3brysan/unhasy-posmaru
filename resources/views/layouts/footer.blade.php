17a84b41edfe470750418f7720f519e79730ff6e
Febry San <97709170+f3brysan@users.noreply.github.com>
2025-09-01 23:36:06 +0700

    feat: update footer with new copyright information and branding
    - Replaced dynamic year generation with Laravel's date helper for consistency.
    - Updated footer branding to reflect new attribution to LLTI UNHASY.
    - Commented out unused links for license and theme resources to streamline the footer layout.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,16 +3,12 @@
                             <div
                                 class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
                                 <div>
-                                    ©
-                                    <script>
-                                        document.write(new Date().getFullYear());
-                                    </script>
-                                    , made with ❤️ by
-                                    <a href="https://pixinvent.com" target="_blank"
-                                        class="footer-link text-primary fw-medium">Pixinvent</a>
+                                    © {{ date('Y') }}, made with ❤️ by
+                                    <a href="javascript:void(0)" target="_blank"
+                                        class="footer-link text-primary fw-medium">LLTI UNHASY</a>
                                 </div>
                                 <div class="d-none d-lg-inline-block">
-                                    <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
+                                    {{-- <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
                                         target="_blank">License</a>
                                     <a href="https://1.envato.market/pixinvent_portfolio" target="_blank"
                                         class="footer-link me-4">More Themes</a>
@@ -21,7 +17,7 @@
                                         target="_blank" class="footer-link me-4">Documentation</a>
 
                                     <a href="https://pixinvent.ticksy.com/" target="_blank"
-                                        class="footer-link d-none d-sm-inline-block">Support</a>
+                                        class="footer-link d-none d-sm-inline-block">Support</a> --}}
                                 </div>
                             </div>
                         </div>
